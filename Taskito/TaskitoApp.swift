d93c1d5e2307ee7283029c52606063f9d170eaeb
Yash Bhalodi <yashbhalodi@toddleapp.com>
2023-02-04 17:24:59 +0530

    Initial Commit

---
+++ Taskito/TaskitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskitoApp.swift
+//  Taskito
+//
+//  Created by Yash Bhalodi on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
